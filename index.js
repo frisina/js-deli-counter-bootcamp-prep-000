de4f6624cf61f83297b21f147b78ebad1b165788
Marco Frisina <marco@frisina.xyz>
2019-05-05 17:03:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(line) {
-  txt = "";
+  var txt = "";
   for(var i = 0; i < line.length; i++) {
     if (line.length < 0) {
       return "The line is currently empty";
