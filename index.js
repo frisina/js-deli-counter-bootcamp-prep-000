b0248e180ab4b424152630a62864861e402da0ef
Marco Frisina <marco@frisina.xyz>
2019-05-05 14:36:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(currentLine, person) {
-  var name = person.pop();
+  var name = person.pop(name);
   return "Welcome, " + name + ". You are number " + (currentLine.length + 1) + " in line.";
 }
