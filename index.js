9912af521544b01317ae46001cc4758bf162079b
Marco Frisina <marco@frisina.xyz>
2019-05-05 15:18:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var katzDeliLine = [];
 function takeANumber(currentLine, person) {
   katzDeliLine.push(person)
   var realName = katzDeliLine.splice(katzDeliLine - 1, 1)
+  var realLength = katzDeliLine.length + 1;
 
-  return "Welcome, " + realName + ". You are number " + katzDeliLine.length + " in line.";
+  return "Welcome, " + realName + ". You are number " + realLength + " in line.";
 }
