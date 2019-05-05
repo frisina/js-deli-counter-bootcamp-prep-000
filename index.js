6c02a52f173cdbac360ee4847ca61aabc6aae58e
Marco Frisina <marco@frisina.xyz>
2019-05-05 17:58:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,16 +27,3 @@ function currentLine(line) {
   }
   return txt;
 }
-
-
-/*function currentLine(line) {
-  var txt = "The line is currently: ";
-  if (line.length === 0) {
-    txt = "The line is currently empty."
-  }
-  for(var i = 0; i < line.length; i++) {
-    txt += i+1 + ". " + line[i] + ", " ;
-    }
-
-  return txt;
-  }
