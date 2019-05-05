6f7725112ea17d00e156478e38d72fd5e1c3583b
Marco Frisina <marco@frisina.xyz>
2019-05-05 16:10:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function takeANumber(katzDeliLine, person) {
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!"
-  } else return "Currently serving " + katzDeliLine[0];
+  } else return "Currently serving " + katzDeliLine[0] +".";
 
   katzDeliLine.shift();
 }
