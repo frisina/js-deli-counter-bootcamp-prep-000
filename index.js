3b29a52c527d5527e351781fc0ea06150185f93b
Marco Frisina <marco@frisina.xyz>
2019-05-05 16:42:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function nowServing(katzDeliLine) {
 
 function currentLine(katzDeliLine) {
   for (var i = 1; i < katzDeliLine.length; i++) {
-    if (katzDeliLine[i] === undefined ) {
+    if (katzDeliLine.length === 0 ) {
       return "The line is currently empty.";
     } else {
     return "The line is currently: " + i + ". " + katzDeliLine[i];
