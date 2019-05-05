0e28b26281559c0a70cf630334e07b83e8822a39
Marco Frisina <marco@frisina.xyz>
2019-05-05 15:11:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,12 +2,6 @@ var katzDeliLine = [];
 
 function takeANumber(currentLine, person) {
   katzDeliLine.push(person)
-  let lastPerson = katzDeliLine.pop();
-  var count = 0;
 
-  for (var i = 0; i < person.length; i++) {
-    count++;
-  }
-
-  return "Welcome, " + lastPerson + ". You are number " + count + " in line.";
+  return "Welcome, " + katzDeliLine[person] + ". You are number " + katzDeliLine.length + " in line.";
 }
