011700b1c1f92b9dd6297c29b8cd8556200d909c
Marco Frisina <marco@frisina.xyz>
2019-05-05 17:58:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,19 @@ function nowServing(katzDeliLine) {
 
 function currentLine(line) {
   var txt = "The line is currently: ";
+  for(var i = 0; i < line.length; i++) {
+    if (line.length === 0) {
+      txt = "The line is currently empty."
+    } else {
+      txt += i+1 + ". " + line[i] + ", " ;
+    }
+  }
+  return txt;
+}
+
+
+/*function currentLine(line) {
+  var txt = "The line is currently: ";
   if (line.length === 0) {
     txt = "The line is currently empty."
   }
