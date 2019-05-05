0343fc2a8a69db9ed3f713f02f28968a48e84fdd
Marco Frisina <marco@frisina.xyz>
2019-05-05 15:42:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ var katzDeliLine = [];
 
 function takeANumber(currentLine, person) {
   katzDeliLine.push(person);
-  var realName = katzDeliLine.splice(katzDeliLine - 1, 1);
 
   console.log(katzDeliLine);
-  return "Welcome, " + realName + ". You are number " + (katzDeliLine.length + 1) + " in line.";
+  return "Welcome, " + person + ". You are number " + (katzDeliLine.length + 1) + " in line.";
 }
